e374f4cdb2d3309818c3a092144c2c5cdb85d297
kcfresh53 <kcfresh53@gmail.com>
2022-11-01 08:59:15 +0100

    Added android Navigation support

--- SceneManager.gd
+++ SceneManager.gd
@@ -12,8 +12,19 @@ signal go_to_prev_scene
 
 
 func _ready():
-# warning-ignore:return_value_discarded
+	# warning-ignore:return_value_discarded
 	connect("go_to_next_scene", self, "_on_go_to_next_scene")
+	
+	# Handles phone back buttons being pressed
+	get_tree().set_quit_on_go_back(false)
+
+
+
+func _notification(what):
+	# When the android back button is pressed, go to the previous scene
+	if what == MainLoop.NOTIFICATION_WM_GO_BACK_REQUEST:
+		emit_signal("go_to_prev_scene")
+
 
 
 func _on_go_to_next_scene(nx_scene, prev_scene):
